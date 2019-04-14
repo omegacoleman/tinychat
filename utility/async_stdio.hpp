d3c94d7292bed1f1b6d74b4524e1a2a2ad1c5e49
maxtorm@win <martinliao1998@gmail.com>
2019-04-14 12:50:55 +0800

    optimize windows readline

--- utility/async_stdio.hpp
+++ utility/async_stdio.hpp
@@ -8,14 +8,12 @@
 #include <boost/asio/io_context.hpp>
 #include <boost/asio/spawn.hpp>
 #include <boost/asio/post.hpp>
-#include <boost/thread.hpp>
-
-#ifdef TINYCHAT_UNIX
-
 #include <boost/asio/buffer.hpp>
-#include <boost/asio/posix/stream_descriptor.hpp>
 #include <boost/asio/read_until.hpp>
-
+#ifdef TINYCHAT_UNIX
+#include <boost/asio/posix/stream_descriptor.hpp>
+#elif defined(TINYCHAT_WIN)
+#include <boost/asio/windows/stream_handle.hpp>
 #endif
 
 #include <iostream>
@@ -24,46 +22,32 @@ namespace tinychat::utility
 {
 
 template <typename CompletionToken>
-auto async_stdin_getline(
-	boost::asio::io_context &ioc,
-	CompletionToken &&token
-)
-{
-	using Result = boost::asio::async_result<CompletionToken, void(boost::system::error_code, std::string)>;
-	using Handler = typename Result::completion_handler_type;
-	Handler handler(std::forward<CompletionToken>(token));
-	Result result(handler);
-#if defined(TINYCHAT_WIN)
-	boost::thread(
-		[&ioc, handler]() mutable
-		{
-			boost::system::error_code ec;
-			std::string context;
-			if ( !std::getline(std::cin, context).good())
+	auto async_stdin_getline(
+		boost::asio::io_context &ioc,
+		CompletionToken &&token
+	)
+	{
+		using Result = boost::asio::async_result<CompletionToken, void(boost::system::error_code, std::string)>;
+		using Handler = typename Result::completion_handler_type;
+		Handler handler(std::forward<CompletionToken>(token));
+		Result result(handler);
+		spawn(
+			ioc,
+			[&ioc, handler](boost::asio::yield_context yield) mutable
 			{
-				ec.assign(GetLastError(), boost::system::system_category());
-			}
-			boost::asio::post(
-				ioc,
-				std::bind(handler, ec, context));
-		}).detach();
+				using namespace boost::asio;
+#if defined(TINYCHAT_WIN)
+				windows::stream_handle asyncable_stdin(ioc, GetStdHandle(STD_INPUT_HANDLE));
 #elif defined(TINYCHAT_UNIX)
-	boost::asio::spawn(
-		ioc,
-		[&ioc, handler](boost::asio::yield_context yield) mutable
-		{
-			using namespace boost::asio;
-			boost::system::error_code ec;
-			std::string context;
-			posix::stream_descriptor asyncable_stdin(ioc, ::dup(STDIN_FILENO));
-			async_read_until(asyncable_stdin, dynamic_buffer(context), '\n', yield[ec]);
-			post(ioc,std::bind(handler,ec,context));
-		});
-#else
-#error Platform currently not supported.
+				posix::stream_descriptor asyncable_stdin(ioc, ::dup(STDIN_FILENO));
 #endif
-	return result.get();
-}
-
+				boost::system::error_code ec;
+				std::string input;
+				async_read_until(asyncable_stdin, dynamic_buffer(input), '\n', yield[ec]);
+				post(ioc, std::bind(handler, ec, input));
+			});
+		return result.get();
+	}
+	
 }
 
